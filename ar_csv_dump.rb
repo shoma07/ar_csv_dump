dafb10458655edda77113c63c3f5ba9cb44aebcb
shoma07 <23730734+shoma07@users.noreply.github.com>
2020-08-15 20:11:30 +0900

    update

--- ar_csv_dump.rb
+++ ar_csv_dump.rb
@@ -5,13 +5,14 @@
 # ダンプファイルを出力する、または取り込む
 require 'optparse'
 require 'csv'
+
 ActiveRecord::Base.logger = nil
 timestamp = Time.now.in_time_zone.strftime('%Y%m%d%H%M%S')
+
 option = {
   dir: "#{Rails.root}/db/dump",
   tables: ActiveRecord::Base.connection.tables
 }
-
 opt = OptionParser.new
 opt.on('-t=VAL', '--table=VAL', Array,
        '対象のテーブル カンマ区切りで複数指定可能') do |a|
@@ -20,26 +21,31 @@ end
 opt.parse!(ARGV)
 
 dump_path = "#{option[:dir]}/#{timestamp}"
-Dir.mkdir(dump_path) unless Dir.exist? dump_path
-option[:table].each do |table|
-  klass = Class.new(ActiveRecord::Base)
-  klass.table_name = table
-  klass.primary_key = klass.connection.primary_keys(table).first
-  content = CSV.generate(force_quotes: true) do |csv|
-    csv << klass.column_names
-    pos = 0
-    range = 1000
-    loop do
-      records = klass.all.order(klass.primary_key).limit(range)
-                     .offset(pos).pluck(*klass.column_names)
-      break if records.empty?
-
-      pos += range
-      records.each do |record|
-        csv << (record.is_a?(Array) ? record : [record])
+Dir.mkdir(dump_path) unless Dir.exist?(dump_path)
+limit = 1000
+option[:tables].each do |table|
+  primary_key = ActiveRecord::Base.connection.primary_keys(table).first
+  klass = Class.new(ActiveRecord::Base) do
+    self.table_name = table
+    self.primary_key = primary_key
+  end
+  column_names = klass.column_names
+  total = klass.all.count
+  File.write(
+    "#{dump_path}/#{table}.csv",
+    CSV.generate(force_quotes: true) do |csv|
+      csv << column_names
+      0.step(total, limit) do |offset|
+        klass.all
+             .order(primary_key)
+             .limit(limit)
+             .offset(offset)
+             .pluck(*column_names)
+             .each do |row|
+          csv << (row.is_a?(Array) ? row : [row])
+        end
       end
     end
-  end
-  File.write("#{dump_path}/#{table}.csv", content)
-  puts "#{table} done!"
+  )
+  puts "#{table}: done!"
 end
